fc4ff42fee8b4bfdca36ec44b583c22f55e16269
Loan <loanbillonnet@gmail.com>
2025-06-27 01:16:04 +0200

    fix: :bug: reset password link

--- src/api/resetPasswordApi.ts
+++ src/api/resetPasswordApi.ts
@@ -1,10 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const resetPassword = async (email: string): Promise<any> => {
   return axios.post(`${API_BASE_URL}/users/reset-password`, { email });
 };
 
-export const confirmResetPassword = async (token: string, password: string): Promise<any> => {
-  return axios.post(`${API_BASE_URL}/users/reset-password/confirm`, { token, password });
+export const confirmResetPassword = async (
+  token: string,
+  password: string
+): Promise<any> => {
+  return axios.post(`${API_BASE_URL}/users/reset-password/confirm`, {
+    token,
+    newPassword: password,
+  });
 };
